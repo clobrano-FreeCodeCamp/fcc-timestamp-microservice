71f4a1539984d604f5958add7d2bebc7cd3889a9
Carlo Lobrano <Carlo.Lobrano@telit.com>
2016-05-20 17:38:43 +0200

    Simplified path

--- app.js
+++ app.js
@@ -22,9 +22,9 @@ app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
-app.use('/timestamp', timestamp);
+//app.use('/', routes);
+//app.use('/users', users);
+app.use('/', timestamp);
 
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
